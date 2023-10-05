25031fb765194f05d15e823c8ec0c328c64c17a7
Natalie <natalie.dflcn@gmail.com>
2023-10-04 20:13:24 -0400

    Started learning events

--- main.js
+++ main.js
@@ -21,3 +21,18 @@ ul.firstElementChild.textContent = 'Hello'; //changes the first element's conten
 ul.children[1].innerText = 'Brad'; //changes the children[1].text to value
 ul.children[0].innerHTML = '<h1>Hello</h1>'; //changes the HTML of children[0]
 
+const btn = document.querySelector('.btn');
+btn.style.background = 'red';
+
+//Events
+btn.addEventListener('click', (event) => {
+    event.preventDefault(); //prevents the form from submitting so you can see what's happening in the console
+   
+    console.log(event.target); //returns info about the element the event is attached to
+    console.log(event.target.className); //returns the class name of target element
+
+    document.querySelector('#my-form').style.background = '#ccc'; //changes the form background color on click
+    document.querySelector('body').classList.add('bg-dark'); //adds the 'bg-dark' css class to the body on click
+
+    document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
+});
